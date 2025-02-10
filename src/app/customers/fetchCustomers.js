1bee134188c76eaa196f8942bbe9bbc9ef3e5dff
ayu1104 <ak3668134@gmail.com>
2025-02-11 07:06:16 +0900

    console.log("Received ID in fetchCustomer:", id); // 追加

--- src/app/customers/fetchCustomers.js
+++ src/app/customers/fetchCustomers.js
@@ -1,4 +1,5 @@
 export default async function fetchCustomers() {
+  console.log("Received ID in fetchCustomer:", id); // 追加
   const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + "/allcustomers", {
     cache: "no-cache",
   });
