23053f9eca8d1209477b02f9ac2cfe26c5693fdc
ayu1104 <ak3668134@gmail.com>
2025-02-11 06:03:49 +0900

    確認用のconsole.logを追加

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,17 @@
+// export default function Home() {
+//   return <h1>Hello World</h1>
+// }
+
+import { useEffect } from "react";
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  useEffect(() => {
+    console.log("API Endpoint:", process.env.NEXT_PUBLIC_API_ENDPOINT);
+  }, []);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
