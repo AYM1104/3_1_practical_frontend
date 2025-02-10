6a8cde502a14ee8b97b3a8fd894f78055d5d37b8
ayu1104 <ak3668134@gmail.com>
2025-02-11 06:29:26 +0900

    クエリパラメータからパスパラメータに変更

--- src/app/customers/read/[id]/fetchCustomer.js
+++ src/app/customers/read/[id]/fetchCustomer.js
@@ -1,6 +1,7 @@
 export default async function fetchCustomer(id) {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`,
+    // process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`,
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers/${id}`, // クエリパラメータからパスパラメータに変更
     { cache: "no-cache" }
   );
   if (!res.ok) {
