fb07009af25c6b00a84ba964ba852a48bbcf35c8
ayu1104 <ak3668134@gmail.com>
2025-02-11 07:09:28 +0900

    ログを確認するために修正

--- src/app/customers/read/[id]/fetchCustomer.js
+++ src/app/customers/read/[id]/fetchCustomer.js
@@ -1,9 +1,17 @@
 export default async function fetchCustomer(id) {
-  const res = await fetch(
-    // process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`,
-    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers/${id}`, // クエリパラメータからパスパラメータに変更
-    { cache: "no-cache" }
-  );
+  console.log("Received ID in fetchCustomer:", id); // 追加
+  
+  const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers/${id}`;
+  console.log("Fetching from:", url); // 追加
+
+  const res = await fetch(url, { cache: "no-cache" });
+
+  // const res = await fetch(
+  //   // process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`,
+  //   `${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers/${id}`, // クエリパラメータからパスパラメータに変更
+  //   { cache: "no-cache" }
+  // );
+
   if (!res.ok) {
     throw new Error("Failed to fetch customer");
   }
